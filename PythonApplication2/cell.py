b610f93be45b4da48c2453e2fd4e262c45dcca78
omaressameldin <o.essameldin@outlook.com>
2016-10-02 22:33:09 +0200

    maze and cell classes -- still not complete

--- PythonApplication2/cell.py
+++ PythonApplication2/cell.py
@@ -1,4 +1,14 @@
+import random;
 class cell(object):
-    """description of class"""
+   def __init__(self, left, up, right, down):
+       self.surroundings = [left,up,right,down];
+       if(left > 1):
+        self.surroundings[0] = random.randint(0,1);
+       if(up > 1):
+        self.surroundings[1] = random.randint(0,1);
+       if(right > 1):
+        self.surroundings[2] = random.randint(0,1);
+       if(down > 1):
+        self.surroundings[3] = random.randint(0,1);
 
 
--- PythonApplication2/maze.py
+++ PythonApplication2/maze.py
@@ -1,4 +1,23 @@
+import random;
 class maze(object):
-    """description of class"""
+
+    def __init__(self):
+        self.columns = random.randint(1,100);
+        self.rows = random.randint(1,100);
+        self.map = [[0 for x in range(self.rows)] for y in range(self.columns)] ;
+        i = 0;
+        for row in range(len(self.map)):
+            for column in range(len(self.map[row])):
+                self.map[row][column] = i;
+
+
+    def printMaze(self):
+        for row in range(len(self.map)):
+            for column in range(len(self.map[row])):
+                print( self.map[row][column]); 
+
+
+x = maze();
+x.printMaze();
 
 
