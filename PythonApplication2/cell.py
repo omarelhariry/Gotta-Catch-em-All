3575ba47a57d3805949553cd3c57737a471621e7
omaressameldin <o.essameldin@outlook.com>
2016-10-05 00:15:26 +0200

    adding pokemon variable to cell and maze generation first draft is now complete

--- PythonApplication2/cell.py
+++ PythonApplication2/cell.py
@@ -10,5 +10,9 @@ class cell(object):
         self.surroundings[2] = random.randint(0,1);
        if(down > 1):
         self.surroundings[3] = random.randint(0,1);
+       self.pokemon = random.randint(0,1);   
 
+
+   def getCell(self):
+    return "["+str(self.surroundings[0]) + ", "+  str(self.surroundings[1])+", "+ str(self.surroundings[2])+", "+ str(self.surroundings[3])+"]";
 
--- PythonApplication2/maze.py
+++ PythonApplication2/maze.py
@@ -1,23 +1,53 @@
 import random;
+from cell import cell;
 class maze(object):
 
     def __init__(self):
-        self.columns = random.randint(1,100);
-        self.rows = random.randint(1,100);
+        self.columns = random.randint(1,10);
+        self.rows = random.randint(1,10);
         self.map = [[0 for x in range(self.rows)] for y in range(self.columns)] ;
         i = 0;
         for row in range(len(self.map)):
             for column in range(len(self.map[row])):
-                self.map[row][column] = i;
+                left = 2;
+                up = 2;
+                right = 2;
+                down = 2;
+                if(column == 0):
+                    left = 1;
+                if(column == self.columns - 1):
+                    right = 1
+                if(row == 0):
+                    up = 1;
+                if(row == self.rows - 1):
+                    down = 1;
+                if(row > 0):
+                    up = self.map[row-1][column].surroundings[3];
+                if(column > 0):
+                    left = self.map[row][column-1].surroundings[2];
+                self.map[row][column] = cell(left,up,right,down);
+
+               
+
+                   
 
 
     def printMaze(self):
+        prevRow = 0;
+        print2 = "";
         for row in range(len(self.map)):
             for column in range(len(self.map[row])):
-                print( self.map[row][column]); 
+                print2 += self.map[row][column].getCell();                 
+                if(row > prevRow):
+                    prevRow = row;
+                    print(print2);
+                    print2 = "";
 
 
 x = maze();
+#x = cell(1,0,0,0);
+#print(x.getCell());
 x.printMaze();
+
 
 
