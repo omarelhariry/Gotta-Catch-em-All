7a6ce76c3be463595ddc6fcfd01691acd601a7ea
omaressameldin <o.essameldin@outlook.com>
2016-10-07 22:18:26 +0200

    random maze complete

--- PythonApplication2/cell.py
+++ PythonApplication2/cell.py
@@ -1,6 +1,6 @@
 import random;
 class cell(object):
-   def __init__(self, left, up, right, down):
+    def __init__(self, left, up, right, down):
        self.surroundings = [left,up,right,down];
        if(left > 1):
         self.surroundings[0] = random.randint(0,1);
@@ -10,9 +10,12 @@ class cell(object):
         self.surroundings[2] = random.randint(0,1);
        if(down > 1):
         self.surroundings[3] = random.randint(0,1);
-       self.pokemon = random.randint(0,1);   
+       self.pokemon = random.randint(0,1); 
+       self.visited = False;  
 
+    def removeWall(self, wallNumber): # 0 --> left 1 --> up 2 --> right 3 --> down
+        self.surroundings[wallNumber] = 0;
 
-   def getCell(self):
-    return "["+str(self.surroundings[0]) + ", "+  str(self.surroundings[1])+", "+ str(self.surroundings[2])+", "+ str(self.surroundings[3])+"]";
+    def getCell(self):
+        return "["+str(self.surroundings[0]) + ", "+  str(self.surroundings[1])+", "+ str(self.surroundings[2])+", "+ str(self.surroundings[3])+"]";
 
--- PythonApplication2/maze.py
+++ PythonApplication2/maze.py
@@ -3,65 +3,100 @@ from cell import cell;
 class maze(object):
 
     def randomize(self):
-        self.columns = random.randint(1,10);
         self.rows = random.randint(1,10);
-        self.map = [[0 for x in range(self.rows)] for y in range(self.columns)] ;
-        i = 0;
+        self.columns = random.randint(1,10);
+        self.map = [[0 for x in range(self.columns)] for y in range(self.rows)] ;
         for row in range(len(self.map)):
             for column in range(len(self.map[row])):
-                left = 2;
-                up = 2;
-                right = 2;
-                down = 2;
-                if(column == 0):
-                    left = 1;
-                if(column == self.columns - 1):
-                    right = 1
-                if(row == 0):
-                    up = 1;
-                if(row == self.rows - 1):
-                    down = 1;
-                if(row > 0):
-                    up = self.map[row-1][column].surroundings[3];
-                if(column > 0):
-                    left = self.map[row][column-1].surroundings[2];
-                self.map[row][column] = cell(left,up,right,down);
+                self.map[row][column] = cell(1,1,1,1);
+        
+        currentCell = random.randint(0,self.rows-1), random.randint(0,self.columns - 1);
+        #print(self.map[9][8].getCell());
+        self.map[currentCell[0]][currentCell[1]].visited = True;
+        unvisitedCells = self.columns * self.rows - 1;
+        cellsStack = [];
+        while unvisitedCells > 0:
+            unVisitedNeighbors = self.getUnVisitedNeighbors(currentCell[0], currentCell[1]);
+            if unVisitedNeighbors:
+                randomUnvisistedCell = unVisitedNeighbors[random.randint(0,len(unVisitedNeighbors) - 1)];
+                cellsStack.append(currentCell);
+                self.map[currentCell[0]][currentCell[1]].removeWall(randomUnvisistedCell);
+                if randomUnvisistedCell == 0:
+                    currentCell = currentCell[0], currentCell[1] - 1;
+                    self.map[currentCell[0]][currentCell[1]].removeWall(2);
+                if randomUnvisistedCell == 1:
+                    currentCell = currentCell[0] - 1, currentCell[1];
+                    self.map[currentCell[0]][currentCell[1]].removeWall(3);
+                if randomUnvisistedCell == 2:
+                    currentCell = currentCell[0], currentCell[1] + 1;
+                    self.map[currentCell[0]][currentCell[1]].removeWall(0);
+                if randomUnvisistedCell == 3:
+                    currentCell = currentCell[0] + 1, currentCell[1];
+                    self.map[currentCell[0]][currentCell[1]].removeWall(1);
+                self.map[currentCell[0]][currentCell[1]].visited = True;
+                unvisitedCells -= 1;
+                print(str(unvisitedCells));
+            elif cellsStack:
+                currentCell = cellsStack.pop();
 
-               
+
+
+
+
+                    
+
+    def getUnVisitedNeighbors(self, i, j):
+        unvisitedNeighbors = [];
+        if j > 0:
+            if not self.map[i][j-1].visited:
+                unvisitedNeighbors.append(0); #left
+        if j < self.columns - 1: 
+            if not self.map[i][j+1].visited:
+                unvisitedNeighbors.append(2); #right
+        if i > 0:
+            if not self.map[i-1][j].visited:
+                unvisitedNeighbors.append(1); #up
+        if i < self.rows - 1:
+            if not self.map[i+1][j].visited:
+                unvisitedNeighbors.append(3); #down
+        return unvisitedNeighbors;
+            
+                                            
+             
+                              
     def constant(self):
         self.columns = 5;
         self.rows = 5;
         self.map = [[0 for x in range(self.rows)] for y in range(self.columns)] ;
         self.map[0][0] =  cell(1,1,0,0);
-        self.map[1][0] =  cell(0,1,1,0);
-        self.map[2][0] =  cell(1,1,0,1);
-        self.map[3][0] =  cell(0,1,0,1);
-        self.map[4][0] =  cell(0,1,1,0);
+        self.map[0][1] =  cell(0,1,1,0);
+        self.map[0][2] =  cell(1,1,0,1);
+        self.map[0][3] =  cell(0,1,0,1);
+        self.map[0][4] =  cell(0,1,1,0);
 
-        self.map[0][1] =  cell(1,0,1,1);
+        self.map[1][0] =  cell(1,0,1,1);
         self.map[1][1] =  cell(1,0,0,0);
-        self.map[2][1] =  cell(0,1,1,0);
-        self.map[3][1] =  cell(1,1,0,1);
-        self.map[4][1] =  cell(0,0,1,0);
+        self.map[1][2] =  cell(0,1,1,0);
+        self.map[1][3] =  cell(1,1,0,1);
+        self.map[1][4] =  cell(0,0,1,0);
 
-        self.map[0][2] =  cell(1,1,0,0);
-        self.map[1][2] =  cell(0,0,1,0);
+        self.map[2][0] =  cell(1,1,0,0);
+        self.map[2][1] =  cell(0,0,1,0);
         self.map[2][2] =  cell(1,0,0,1);
-        self.map[3][2] =  cell(0,1,1,0);
-        self.map[4][2] =  cell(1,0,1,0);
+        self.map[2][3] =  cell(0,1,1,0);
+        self.map[2][4] =  cell(1,0,1,0);
 
-        self.map[0][3] =  cell(1,0,1,1);
-        self.map[1][3] =  cell(1,0,0,0);
-        self.map[2][3] =  cell(0,1,1,1);
+        self.map[3][0] =  cell(1,0,1,1);
+        self.map[3][1] =  cell(1,0,0,0);
+        self.map[3][2] =  cell(0,1,1,1);
         self.map[3][3] =  cell(1,0,1,0);
-        self.map[4][3] =  cell(1,0,1,0);
+        self.map[3][4] =  cell(1,0,1,0);
 
-        self.map[0][4] =  cell(1,1,0,1);
-        self.map[1][4] =  cell(0,0,1,1);
-        self.map[2][4] =  cell(1,1,0,1);
-        self.map[3][4] =  cell(0,0,0,1);
+        self.map[4][0] =  cell(1,1,0,1);
+        self.map[4][1] =  cell(0,0,1,1);
+        self.map[4][2] =  cell(1,1,0,1);
+        self.map[4][3] =  cell(0,0,0,1);
         self.map[4][4] =  cell(0,0,1,1);
-
 
 
 
@@ -69,22 +104,22 @@ class maze(object):
 
 
     def printMaze(self):
-        prevRow = 0;
-        print2 = "";
         for row in range(len(self.map)):
+            toBePrinted = [];
             for column in range(len(self.map[row])):
-                print2 += self.map[row][column].getCell();                 
-                if(row > prevRow):
-                    prevRow = row;
-                    print(print2);
-                    print2 = "";
+                toBePrinted.append(self.map[row][column].getCell());                 
+                if(column == self.columns - 1):
+                    print(toBePrinted);
+
 
 
 x = maze();
-#x = cell(1,0,0,0);
+#x = cell(1,0,1,0);
 #print(x.getCell());
-x.constant();
+x.randomize();
+#x.map[0][0].removeWall(0);
+#print(x.map[88][98].getCell());
 x.printMaze();
-
+#print(x.map[8][9].getCell());
 
 
